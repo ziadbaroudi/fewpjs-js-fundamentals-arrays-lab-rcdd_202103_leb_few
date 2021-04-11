5c717254ac9404052ae3f4e0c211fa052ba02410
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 17:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.append("Odie")
+append.push("Odie")
