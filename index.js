c2752582ca26da569f164c90d2ead75471f7a8a8
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 17:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.unshift("Odie")
